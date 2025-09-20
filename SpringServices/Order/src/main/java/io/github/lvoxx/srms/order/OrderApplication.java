b4c2443bba43aadccbb17cf90128f113dba416d9
lvoxx <lvoxxartist@gmail.com>
2025-09-20 15:44:29 +0700

    Fix r2dbc inject

--- SpringServices/Order/src/main/java/io/github/lvoxx/srms/order/OrderApplication.java
+++ SpringServices/Order/src/main/java/io/github/lvoxx/srms/order/OrderApplication.java
@@ -3,7 +3,7 @@ package io.github.lvoxx.srms.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.github.lvoxx.srms")
 public class OrderApplication {
 
 	public static void main(String[] args) {
