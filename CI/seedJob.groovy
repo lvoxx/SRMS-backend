10ca4958f012d79abbf3132b0d3da3c8e4b74e84
lvoxx <lvoxxartist@gmail.com>
2025-10-27 15:35:19 +0700

    Updated seedJob for loading correct jobs and config

--- CI/seedJob.groovy
+++ CI/seedJob.groovy
@@ -1,3 +1,11 @@
-new File('CI/jobs').eachFileMatch(~/.*\.groovy/) { file ->
-    evaluate(file)
+// Load jobs
+new File("${WORKSPACE}/CI/jobs").eachFileMatch(~/.*\.groovy/) { file ->
+    println "Loading job: ${file.name}"
+    evaluate(file.text)
+}
+
+// Load configs
+new File("${WORKSPACE}/CI/config").eachFileMatch(~/.*\.groovy/) { file ->
+    println "Loading config: ${file.name}"
+    evaluate(file.text)
 }
