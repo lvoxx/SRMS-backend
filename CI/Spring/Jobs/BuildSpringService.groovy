ed7bc830febce20ebb73501a301eff2f630ec04a
lvoxx <lvoxxartist@gmail.com>
2025-10-29 17:53:29 +0700

    Removed all old shit CI

--- CI/Spring/Jobs/BuildSpringService.groovy
+++
@@ -1,7 +0,0 @@
-SharedJobDSL.createJobFromTemplate(this, [
-    name: 'Build-Spring-Service',
-    stages: [
-        { "stage('Build') { steps { dir(env.CODEBASE_DIR) { sh \"mvn -B clean package -DskipTests -pl ${params.SERVICE_NAME} -am\" } } }" },
-        { "stage('Archive') { steps { archiveArtifacts \"${env.CODEBASE_DIR}/${params.SERVICE_NAME}/target/*.jar\" } }" }
-    ]
-])
--- CI/Spring/Jobs/TestSpringService.groovy
+++
@@ -1,7 +0,0 @@
-SharedJobDSL.createJobFromTemplate(this, [
-    name: 'Test-Spring-Services',
-    stages: [
-        { "stage('Test') { steps { dir(env.CODEBASE_DIR) { sh 'mvn -B clean test' } } }" },
-        { "stage('Report') { steps { junit '${env.CODEBASE_DIR}/**/surefire-reports/*.xml' } }" }
-    ]
-])
