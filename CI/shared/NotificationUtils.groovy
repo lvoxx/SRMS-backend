c25678a74f8b5bf0d44903412a14bed93abccd53
lvoxx <lvoxxartist@gmail.com>
2025-10-29 15:53:18 +0700

    Removed all old compicated jobs

--- CI/shared/NotificationUtils.groovy
+++
@@ -1,9 +0,0 @@
-// Notification Utilities
-def notifyBuildStatus(config, status) {
-    if ('slack' in config.notify) {
-        slackSend(channel: '#ci-channel', message: "Build ${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
-    }
-    if ('email' in config.notify) {
-        emailext subject: "Build ${status}", body: "...", to: 'team@email.com'
-    }
-}
